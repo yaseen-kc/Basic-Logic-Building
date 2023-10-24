c4897290a9326570021581f6a9dbc21fd66764fc
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-24 18:43:41 +0530

    .

--- test.c
+++ test.c
@@ -1,34 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int n;
-    printf("Enter the number of values: ");
+
+    printf("Enter the number of rows: ");
     scanf("%d", &n);
 
-    int arr[n];
-
-    // Read values into the array
-    printf("Enter %d values:\n", n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    // Display the array after skipping two values next to multiples of 5
-    printf("Array after skipping two values next to multiples of 5:\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] % 5 == 0)
-        {
-            printf("%d ", arr[i]); // Print the multiple of 5
-            i += 2;                // Skip the next two values
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ",j ); //i * j
         }
-        else
-        {
-            printf("%d ", arr[i]);
-        }
+        printf("\n");
     }
 
     return 0;
