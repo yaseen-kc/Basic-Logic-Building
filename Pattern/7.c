3fc5fe7a75b7a30048b27a1ebf006120234ac0e1
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-25 14:38:32 +0530

    .

--- Pattern/7.c
+++ Pattern/7.c
@@ -3,17 +3,23 @@
 
 int main(void)
 {
-    int i, j, k;
-
-    for (i = 1; i <= 4; i++)
+    int i, j, k, n = 4, c = 7;
+    for (i = 1; i <= n; i++)
     {
         k = 1;
-        for (j = 1; j <= 7; i++)
+        for (j = 1; j <= c; j++)
         {
-            if (j >= 5 - i && j <= 3 + i)
+            if (j >= n + 1 - i && j <= n - 1 + i)
             {
                 printf("%d ", k);
-                k++;
+                if (j < 4)
+                {
+                    k++;
+                }
+                else
+                {
+                    k--;
+                }
             }
             else
             {
