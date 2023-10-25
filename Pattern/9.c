ff090ae120f3b6100dc419344d767eef5bd1db2e
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-25 20:18:04 +0530

    .

--- Pattern/9.c
+++ Pattern/9.c
@@ -3,15 +3,25 @@
 
 int main(void)
 {
-    int i, j, k;
+    int i, j;
+    int k = 0;
     int n = 7;
     int c = 7;
 
     for (i = 1; i <= n; i++)
     {
+        if (i <= 4)
+        {
+            k++;
+        }
+        else
+        {
+            k--;
+        }
+
         for (j = 1; j <= c; j++)
         {
-            if (j >= 5 - i && j <= 3 + i)
+            if (j >= 5 - k && j <= 3 + k)
             {
                 printf("* ");
             }
