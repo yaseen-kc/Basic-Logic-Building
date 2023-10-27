741076704da95f0acbddd4f05306970a2653c6ab
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-27 20:43:30 +0530

    .

---
+++ temp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, j, k;
+    int n = 4, c = 7;
+    for (i = 1; i <= n; i++)
+    {
+        k = 1;
+        for (j = 1; j <= c; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+                printf("%d ", k);
+                if (k < 4)
+                {
+                    k++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
