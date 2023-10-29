4069856257b8fb1aac02270299de380f3cf30f90
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-29 13:27:24 +0530

    .

--- temp.c
+++ temp.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+// 342
+int main(void)
+{
 
-int main() {
-    int array[] = {4, 2, 4, 5, 2, 3, 6, 7, 4, 3};
-    int size = sizeof(array) / sizeof(array[0]);
+    int i, j;
+    int n = 7;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            // i == 0 || j == 0 || i == n - 1 || i == j || j == n - 1 || i + j == n - 1
 
-    printf("Repeated elements and their counts:\n");
-
-    for (int i = 0; i < size; i++) {
-        int count = 1;  // Initialize count to 1 for the current element.
-
-        if (array[i] == -1) {
-            continue;  // Element already counted, move to the next.
-        }
-
-        for (int j = i + 1; j < size; j++) {
-            if (array[i] == array[j]) {
-                count++;
-                array[j] = -1;  // Mark the element as counted.
+            if (i == 1 || j == 1 || i == n || j == n || j == i || j == 8 - i)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
             }
         }
-
-        if (count > 1) {
-            printf("%d (count: %d)\n", array[i], count);
-        }
+        printf("\n");
     }
-
-    return 0;
 }
