bc2f7168529c49a0e50418cec9d90b577a1bd989
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-30 13:20:23 +0530

    .

--- OOP/ClassObject/src/test.java
+++ OOP/ClassObject/src/test.java
@@ -1,10 +1,24 @@
 public class test {
 
-    static int findSquare(int a) {
-        return a * a;
+    static int findSum(int a, int b) {
+        return a + b;
+    }
+
+    static int findSum(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    static double findSum(double i, double j) {
+        return i + j;
     }
 
     public static void main(String[] ar) {
-        System.out.println(findSquare(8));
+        int sum1 = findSum(1, 2);
+        int sum2 = findSum(1, 2, 3);
+        double sum3 = findSum(45.6, 78.3);
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
     }
 }
