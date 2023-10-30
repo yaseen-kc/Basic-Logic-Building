11d24bb288a45f7f7ea37cbf2c675c874f178fca
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-30 17:40:40 +0530

    .

--- OOP/ClassObject/src/test.java
+++ OOP/ClassObject/src/test.java
@@ -1,24 +1,25 @@
+class Student {
+
+    int rollno;
+    String name;
+    static String college = "ITS";
+
+    Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
+
 public class test {
+    public static void main(String args[]) {
+        Student s1 = new Student(111, "Karan");
+        Student s2 = new Student(222, "Aryan");
 
-    static int findSum(int a, int b) {
-        return a + b;
-    }
-
-    static int findSum(int x, int y, int z) {
-        return x + y + z;
-    }
-
-    static double findSum(double i, double j) {
-        return i + j;
-    }
-
-    public static void main(String[] ar) {
-        int sum1 = findSum(1, 2);
-        int sum2 = findSum(1, 2, 3);
-        double sum3 = findSum(45.6, 78.3);
-
-        System.out.println(sum1);
-        System.out.println(sum2);
-        System.out.println(sum3);
+        s1.display();
+        s2.display();
     }
 }
