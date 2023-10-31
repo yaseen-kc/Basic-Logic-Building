f61af94376a2a188ca63bf4ba6487b430e0e6f60
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2023-10-31 18:44:29 +0530

    .

--- test.c
+++ test.c
@@ -1,49 +1,19 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-
-    int i, j, x;
-    int n = 5;
-
-    for (i = 1; i <= n; i++)
+    int i, j, k;
+    for (i = 1; i <= 5; i++)
     {
-        x = 1;
-        for (j = 1; j <= 9; j++)
+        for (j = 1; j < i; j++)
         {
-            if (j >= i && j <= 10 - i && x)
-            {
-                printf("* ");
-                x = 0;
-            }
-            else
-            {
-                printf("  ");
-                x = 1;
-            }
+            printf("  ");
         }
-        printf("\n");
-    }
-
-    int k, l;
-    int m = 4;
-
-    for (k = 1; k <= m; k++)
-    {
-        x = 1;
-        for (l = 1; l <= 9; l++)
+        for (k = 5; k >= i; k--)
         {
-            if (l >= 5 - k && l <= 5 + k && x)
-            {
-                printf("* ");
-                x = 0;
-            }
-            else
-            {
-                printf("  ");
-                x = 1;
-            }
+            printf("*   ");
         }
         printf("\n");
     }
